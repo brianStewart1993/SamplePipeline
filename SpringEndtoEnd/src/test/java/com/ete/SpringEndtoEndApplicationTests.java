5260cb15cacf9a97358ef8df7b21757d7b687274
brianStewart1993 <stewart.brian36@yahoo.com>
2018-06-07 16:25:52 -0500

    Update SpringEndtoEndApplicationTests.java

--- SpringEndtoEnd/src/test/java/com/ete/SpringEndtoEndApplicationTests.java
+++ SpringEndtoEnd/src/test/java/com/ete/SpringEndtoEndApplicationTests.java
@@ -93,7 +93,7 @@ public class SpringEndtoEndApplicationTests {
 
 	    @Test
 	    public void Register() throws Exception {
-	        User user = new User(44, "testingsagain", "testdatacus");
+	        User user = new User(44, "testagain", "testdatacus");
 	        String bookmarkJson = json(user);
 
 	        this.mockMvc.perform(post("/users/addCustom")
@@ -113,7 +113,7 @@ public class SpringEndtoEndApplicationTests {
 	    @Test
 	    public void deleteUser() throws Exception {
 
-	        this.mockMvc.perform(delete("/users/delete/57"))
+	        this.mockMvc.perform(delete("/users/delete/59"))
 	        .andExpect(status().isOk())
             .andExpect(content().contentType("text/plain;charset=UTF-8"));
 	    }
