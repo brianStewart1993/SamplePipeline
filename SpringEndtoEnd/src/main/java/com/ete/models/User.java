3b6895a0b08204b715bcff64fcd4a2e89b48342c
Brian Stewart <brian.stewart@ingenuitytech.co>
2018-06-14 10:38:24 -0500

    Removed unused code

--- SpringEndtoEnd/src/main/java/com/ete/models/User.java
+++ SpringEndtoEnd/src/main/java/com/ete/models/User.java
@@ -24,11 +24,6 @@ public class User {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public User(String username, String password) {
-			this.username = username;
-			this.password = password;
-		}
-
 	public String getUsername() {
 		return username;
 	}
