e9556d259cf43848c66bdc3f793e1b1980c28b03
Brian Stewart <brian.stewart@ingenuitytech.co>
2018-06-23 17:33:38 -0500

    Added Performance Testing

--- SpringEndtoEnd/src/main/java/com/ete/controllers/UserController.java
+++ SpringEndtoEnd/src/main/java/com/ete/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -188,4 +189,12 @@ public class UserController {
 			return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
 		}
 	}
+	
+	@GetMapping("/io")
+    public String io() throws InterruptedException {
+        long sleepDuration = 200L + (long) (500L * Math.random());
+        Thread.sleep(sleepDuration);
+        return "done after " + sleepDuration + "ms";
+    }
+
 }
