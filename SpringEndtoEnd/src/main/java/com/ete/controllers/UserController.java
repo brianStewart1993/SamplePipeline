c299f226f83e6a9cdcfb883ec9ed63feee37ae6c
Brian Stewart <brian.stewart@ingenuitytech.co>
2018-07-19 10:19:53 -0500

    Sonar added

--- SpringEndtoEnd/src/main/java/com/ete/controllers/UserController.java
+++ SpringEndtoEnd/src/main/java/com/ete/controllers/UserController.java
@@ -1,9 +1,6 @@
 package com.ete.controllers;
 
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
